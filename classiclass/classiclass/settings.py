a4826c47f595161f5b2e5fae8dc049289fe734d0
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-04 19:54:44 +0900

    update settings : add debug_toolbar, django_extensions

--- classiclass/classiclass/settings.py
+++ classiclass/classiclass/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_toolbar',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
