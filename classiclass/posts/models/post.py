eceee3e5a30426196498f8b36a7faba5368ab751
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-07 14:42:47 +0900

    post model tag model and signal

---
+++ classiclass/posts/models/post.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+from tags.models import Tag
+
+
+class Post(models.Model):
+    hash_id = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+        unique=True,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+    )
+    image = models.ImageField()
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True, )
+
+    tag_set = models.ManyToManyField(
+        Tag,
+    )
+
+    def init_hash_id(self):
+        from classiclass.utils.hash_id import get_encoded_hash_id
+
+        self.hash_id = get_encoded_hash_id(self)
+        self.save()
