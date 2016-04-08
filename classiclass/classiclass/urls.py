96e9b521a2ca46a34787d0d25fa089c8191e57d0
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-08 17:32:18 +0900

    update hash_id  on detail page

--- classiclass/classiclass/urls.py
+++ classiclass/classiclass/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', PostListView.as_view(), name="posts"),
     url(r'^home/$', HomeView.as_view(), name="home"),
-    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name="post"),
+    url(r'^(?P<slug>\w+)/$', PostDetailView.as_view(), name="post"),
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^logout/$', LogoutView.as_view(), name="logout"),
     url(r'^signup/$', SignupView.as_view(), name="signup"),
--- classiclass/classiclass/utils/hash_id.py
+++ classiclass/classiclass/utils/hash_id.py
@@ -5,7 +5,7 @@ def get_encoded_hash_id(instance):
 
     hashids_object = Hashids(
         salt=instance._meta.model_name,
-        min_length=4,
+        min_length=6,
     )
     hash_id = hashids_object.encode(instance.id)
     return hash_id
