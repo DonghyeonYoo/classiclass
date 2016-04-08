2ab8af0a96801104716da53e43e5a3a995a8235d
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-08 22:24:32 +0900

    init TagDetailView

--- classiclass/classiclass/urls.py
+++ classiclass/classiclass/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from users.views import *
 from posts.views import *
+from tags.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,5 +15,6 @@ urlpatterns = [
     url(r'^signup/$', SignupView.as_view(), name="signup"),
     url(r'^posts/(?P<slug>\w+)/$', PostDetailView.as_view(), name="post"),
     url(r'^posts/(?P<slug>\w+)/tags/$', PostTagCreateView.as_view(), name="post-tags"),
+    url(r'^filtering/tags/(?P<slug>\w+)/$', TagDetailView.as_view(), name="tag"),
     url(r'^(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
