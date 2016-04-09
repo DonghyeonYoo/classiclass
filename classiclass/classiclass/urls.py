46c9532b2461471194c42f58af2332015f458020
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-10 07:06:37 +0900

    init PostDetailAPIView

--- classiclass/classiclass/urls.py
+++ classiclass/classiclass/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from users.views import *
 from posts.views import *
 from tags.views import *
+from posts.api import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,8 +15,9 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^logout/$', LogoutView.as_view(), name="logout"),
     url(r'^signup/$', SignupView.as_view(), name="signup"),
-    url(r'^posts/(?P<slug>\w+)/$', PostDetailView.as_view(), name="post"),
     url(r'^posts/(?P<slug>\w+)/tags/$', PostTagCreateView.as_view(), name="post-tags"),
+    url(r'^posts/(?P<slug>\w+)/$', PostDetailView.as_view(), name="post"),
+    url(r'^api/posts/(?P<hash_id>\w+)/$', PostDetailAPIView.as_view(), name="api-post"),
     url(r'^filtering/tags/(?P<slug>\w+)/$', TagDetailView.as_view(), name="tag"),
     url(r'^(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ classiclass/posts/serializers/post.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from posts.models import Post
+
+from .tag import TagModelSerializer
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    tags = TagModelSerializer(source="tag_set.all", many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'content',
+            'tags',
+        )
