f922ffcd66d507882e01218d2347bfeb9ec79021
RDH <RDH@RDHui-MacBook-Air.local>
2016-04-11 02:51:36 +0900

    update datapage dbSqlite  -> postgresql

--- classiclass/classiclass/settings/partials/database.py
+++ classiclass/classiclass/settings/partials/database.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .application import BASE_DIR
 
 
@@ -7,8 +9,7 @@ from .application import BASE_DIR
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+    )
 }
